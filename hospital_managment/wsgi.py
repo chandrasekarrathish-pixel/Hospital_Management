a58c42ebd4c14758e187e782ddd937400c46cfc0
Rathish <chandrasekarrathish@gmail.com>
2026-03-31 17:09:34 +0530

    Switch to psycopg2-binary and add vercel config

--- hospital_managment/wsgi.py
+++ hospital_managment/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_managment.settings')
 
 application = get_wsgi_application()
+app = application
